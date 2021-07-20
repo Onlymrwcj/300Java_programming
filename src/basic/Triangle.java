6b5699a8139f2b4c8ccff64fe349358ce83f9d17
归晴 <1044197604@qq.com>
2021-07-20 21:41:47 +0800

    Create Triangle.java

---
+++ src/basic/Triangle.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Triangle {
+    public static void main(String[] args) {
+        System.out.println(" *");
+        System.out.println(" ***");
+        System.out.println(" *****");
+        System.out.println(" *******");
+    }
+}
